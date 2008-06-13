43f924cfe8696609422c57de29b378accbc87fb2
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2008-06-13 21:56:55 +0000

    Typen auf T geändert

--- oxtorne/source/geometry.c
+++ oxtorne/source/geometry.c
@@ -32,7 +32,7 @@ vector3d OX_v3d_cross (const vector3d* _p1, const vector3d* _p2) {
 	return _result;
 }
 
-vector3d OX_v3d_times (const vector3d* _p, const double _d) {
+vector3d OX_v3d_times (const vector3d* _p, const T _d) {
 	vector3d _vector;
 	_vector.x = _p->x * _d;
 	_vector.y = _p->y * _d;
@@ -42,24 +42,24 @@ vector3d OX_v3d_times (const vector3d* _p, const double _d) {
 
 vector3d OX_v3d_normalize (const vector3d* _p) {
 	vector3d _result;
-	double _sum = _p->x + _p->y + _p->z;
+	T _sum = _p->x + _p->y + _p->z;
 	_result.x = _p->x / _sum;
 	_result.y = _p->y / _sum;
 	_result.z = _p->z / _sum;
 	return _result;
 }
 
-double OX_v3d_dot (const vector3d* _p1, const vector3d* _p2) {
+T OX_v3d_dot (const vector3d* _p1, const vector3d* _p2) {
 	return (_p1->x * _p2->x) + (_p1->y * _p2->y) + (_p1->z * _p2->z);
 }
 
-double OX_v3d_distance (const vector3d* _p1, const vector3d* _p2) {
+T OX_v3d_distance (const vector3d* _p1, const vector3d* _p2) {
 	return sqrt(pow(_p1->x - _p2->x, 2.0) +
 	            pow(_p1->y - _p2->y, 2.0) +
 	            pow(_p1->z - _p2->z, 2.0));
 }
 
-double OX_l3d_length (const line3d* _line) {
+T OX_l3d_length (const line3d* _line) {
 	return OX_v3d_distance(&_line->p, &_line->p);
 }
 
@@ -71,28 +71,28 @@ int OX_lp3d_intersect (const line3d* _line, const plane3d* _plane) {
 }
 
 int OX_rp3d_intersect (const ray3d* _ray, const plane3d* _plane) {
-    double _projection = OX_v3d_dot(&_ray->origin, &_plane->normal);
-    double _unit = OX_v3d_dot(&_ray->direction, &_plane->normal);
+    T _projection = OX_v3d_dot(&_ray->origin, &_plane->normal);
+    T _unit = OX_v3d_dot(&_ray->direction, &_plane->normal);
     return ((_projection - _plane->scalar) / _unit > 0.0);
 }
 
 vector3d OX_rp3d_intersection_point (const ray3d* _ray, const plane3d* _plane) {
-    double _projection = OX_v3d_dot  (&_ray->origin,    &_plane->normal);
-    double _unit       = OX_v3d_dot  (&_ray->direction, &_plane->normal);
+    T _projection = OX_v3d_dot  (&_ray->origin,    &_plane->normal);
+    T _unit       = OX_v3d_dot  (&_ray->direction, &_plane->normal);
     vector3d _intersec = OX_v3d_times(&_ray->direction, (_projection - _plane->scalar) / _unit);
     return OX_v3d_plus(&_ray->origin, &_intersec);
 }
 
 vector3d OX_closest_point_on_ray_from_point (const ray3d* _ray, const vector3d* _vector) {
     vector3d _a        = OX_v3d_minus(_vector, &_ray->origin);
-    double _projection = OX_v3d_dot(&_a,       &_ray->direction);
+    T _projection = OX_v3d_dot(&_a,       &_ray->direction);
     if (_projection < 0)
         return _ray->origin;
     _a =  OX_v3d_times(&_ray->direction, _projection);
     return OX_v3d_plus(&_ray->origin, &_a);
 }
 
-vector3d OX_make_vector3d (const double _x, const double _y, const double _z) {
+vector3d OX_make_vector3d (const T _x, const T _y, const T _z) {
 	vector3d _vector;
 	_vector.x = _x;
 	_vector.y = _y;
@@ -100,7 +100,7 @@ vector3d OX_make_vector3d (const double _x, const double _y, const double _z) {
 	return _vector;
 }
 
-sphere OX_make_sphere (const double _x, const double _y, const double _z, const double _radius) {
+sphere OX_make_sphere (const T _x, const T _y, const T _z, const T _radius) {
 	sphere _sphere;
 	_sphere.x = _x;
 	_sphere.y = _y;
@@ -109,8 +109,8 @@ sphere OX_make_sphere (const double _x, const double _y, const double _z, const 
 	return _sphere;
 }
 
-line3d OX_make_line3d (const double _x1, const double _y1, const double _z1,
-                       const double _x2, const double _y2, const double _z2) {
+line3d OX_make_line3d (const T _x1, const T _y1, const T _z1,
+                       const T _x2, const T _y2, const T _z2) {
 	line3d _line;
 	_line.p.x = _x1;
 	_line.p.y = _y1;
@@ -121,8 +121,8 @@ line3d OX_make_line3d (const double _x1, const double _y1, const double _z1,
 	return _line;
 }
 
-ray3d OX_make_ray3d (const double _x1, const double _y1, const double _z1,
-                     const double _x2, const double _y2, const double _z2) {
+ray3d OX_make_ray3d (const T _x1, const T _y1, const T _z1,
+                     const T _x2, const T _y2, const T _z2) {
 	ray3d _ray;
 	_ray.origin.x = _x1;
 	_ray.origin.y = _y1;
--- oxtorne/source/geometry.h
+++ oxtorne/source/geometry.h
@@ -7,39 +7,23 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-typedef struct vector3d     {
-    double x, y, z;
-} vector3d;
+typedef double T;
 
-typedef struct sphere {
-    double x, y, z, radius;
-} sphere;
-
-typedef struct line3d {
-    vector3d p, q;
-} line3d;
-
-typedef struct ray3d {
-    vector3d origin, direction;
-} ray3d;
-
-typedef struct plane3d {
-    vector3d normal;
-    double scalar;
-} plane3d;
-
-typedef struct triangle3d {
-    vector3d a, b, c;
-} triangle3d;
+typedef struct vector3d		{ T x, y, z;					} vector3d;
+typedef struct sphere		{ T x, y, z, radius;			} sphere;
+typedef struct line3d		{ vector3d p, q;				} line3d;
+typedef struct ray3d		{ vector3d origin, direction;	} ray3d;
+typedef struct plane3d		{ vector3d normal; T scalar;	} plane3d;
+typedef struct triangle3d	{ vector3d a, b, c;				} triangle3d;
 
 vector3d
     OX_v3d_plus                 (const vector3d*, const vector3d*),
     OX_v3d_minus                (const vector3d*, const vector3d*),
     OX_v3d_cross                (const vector3d*, const vector3d*),
-    OX_v3d_times                (const vector3d*, const double),
+    OX_v3d_times                (const vector3d*, const T),
     OX_v3d_normalize            (const vector3d*);
 
-double
+T
     OX_v3d_dot                  (const vector3d*, const vector3d*),
     OX_v3d_distance             (const vector3d*, const vector3d*),
     OX_l3d_length               (const line3d*);
@@ -53,17 +37,17 @@ vector3d
     OX_closest_point_on_ray_from_point (const ray3d*, const vector3d*);
 
 vector3d
-    OX_make_vector3d            (const double, const double, const double);
+    OX_make_vector3d            (const T, const T, const T);
 
 sphere
-    OX_make_sphere              (const double, const double, const double, const double);
+    OX_make_sphere              (const T, const T, const T, const T);
 
 line3d
-    OX_make_line3d              (const double, const double, const double,
-                                 const double, const double, const double);
+    OX_make_line3d              (const T, const T, const T,
+                                 const T, const T, const T);
 
 ray3d
-    OX_make_ray3d               (const double, const double, const double,
-                                 const double, const double, const double);
+    OX_make_ray3d               (const T, const T, const T,
+                                 const T, const T, const T);
 
 #endif
