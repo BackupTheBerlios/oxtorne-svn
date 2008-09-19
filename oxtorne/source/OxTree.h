ff4c6511a390ce009bcc2a3267b252be46aa0006
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2008-09-19 21:11:45 +0000

    new oxtorne tree

--- oxtorne/source/OxTree.h
+++ oxtorne/source/OxTree.h
@@ -9,24 +9,52 @@ class node {
 public:
     typedef       typename std::vector<node<T,D>*>::iterator iterator;
     typedef const typename std::vector<node<T,D>*>::const_iterator const_iterator;
-    typedef       typename T& reference;
-    typedef const typename T& const_reference;
+    typedef       T& reference;
+    typedef const T& const_reference;
+    typedef const std::size_t size_t;
 
-    node() : children(D) {}
-   ~node() {}
+    node() : children(D,0) {}
+    node(const node<T,D>& _node) : children(D,0) { copy_constructor(_node); }
+   ~node() { clear(); }
 
-    inline iterator         begin()                                    { return children.begin(); }
-    inline const_iterator   begin()                              const { return children.begin(); }
-    inline iterator         end()                                      { return children.end();   }
-    inline const_iterator   end()                                const { return children.end();   }
-    inline reference&       operator[](const std::size_t& index)       { return children[index];  }
-    inline const_reference& operator[](const std::size_t& index) const { return children[index];  }
-    inline std::size_t      size()                                     { return D;                }
-
+    inline iterator         begin()                         { return children.begin(); }
+    inline const_iterator   begin()                   const { return children.begin(); }
+    inline iterator         end()                           { return children.end();   }
+    inline const_iterator   end()                     const { return children.end();   }
+    inline node<T,D>*       operator[](size_t& index)       { return children[index];  }
+    inline const node<T,D>* operator[](size_t& index) const { return children[index];  }
+    inline std::size_t      size()                          { return D;                }
+    
     T value;
 
 private:
     std::vector<node<T,D>*> children;
+    
+    inline void copy_constructor(const node<T,D>& _node) {
+        for (int i = 0; i < D; ++i)
+            if (_node[i] != 0) children[i] = new node<T,D>(_node[i]);
+    }
+
+public:
+    inline void assign(size_t& index, const T& val) {
+        if (children[index])
+            children[index]->clear();
+        else
+            children[index] = new node<T,D>();
+        children[index]->value = val;
+    }
+    
+    inline void clear() {
+        for (int i = 0; i < D; ++i)
+            if (children[i]) delete(children[i]);
+    }
+    
+    inline bool leaf() {
+        bool leaf = true;
+        for (int i = 0; i < D; ++i)
+            leaf = leaf && !children[i];
+        return leaf;
+    }
 
 };
 
--- oxtorne/source/TreeTest.cpp
+++ oxtorne/source/TreeTest.cpp
@@ -12,7 +12,16 @@ int main(int argc, char* argv) {
     node<int,2>::iterator n_iter = _node.begin();
     node<int,2>::iterator n_end  = _node.end();
     
+    _node.value = 42;
+    _node.assign(0, 16);
+    _node.assign(1, 32);
+    
+    
     for (; n_iter != n_end; n_iter++) {
-        std::cout << *n_iter << std::endl;
+        std::cout << "by iterator " << n_iter->value << std::endl;
+    }
+    
+    for (int i = 0; i < _node.size(); ++i) {
+        std::cout << "by index " << _node[i]->value << std::endl;
     }
 }
