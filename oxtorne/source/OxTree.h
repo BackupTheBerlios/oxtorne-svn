81e256af67b6881e8c6d25f800092191e11142a3
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2008-09-25 15:52:25 +0000

    datenstruktur fertig gestellt

--- oxtorne/source/OxTree.h
+++ oxtorne/source/OxTree.h
@@ -1,7 +1,6 @@
 
 #include <vector>
 
-
 /* Node Type ******************************************************************/
 template<typename T, std::size_t D>
 class node {
@@ -10,17 +9,50 @@ public:
     typedef       T& reference;
     typedef const T& const_reference;
 
+    class iterator : public std::iterator< std::bidirectional_iterator_tag, node<T,D> > {
+    public:
+        iterator() {}
+       ~iterator() {}
+
+        inline iterator&  operator++()                      { ++node; return *this;      }
+        inline iterator   operator++(int)                   { iterator last = *this; ++*this; return last; }
+        inline iterator&  operator--()                      { --node; return *this;      }
+        inline iterator   operator--(int)                   { iterator last = *this; --*this; return last; }
+        inline bool       operator==(const iterator& other) { return node == other.node; }
+        inline bool       operator!=(const iterator& other) { return node != other.node; }
+        inline node<T,D>& operator* ()                      { return **node;             }
+        inline node<T,D>* operator->()                      { return  *node;             }
+
+        node<T,D>** node;
+    };
+
+    class const_iterator : public std::iterator< std::bidirectional_iterator_tag, node<T,D> > {
+    public:
+        const_iterator() {}
+       ~const_iterator() {}
+
+        inline bool       operator==(const_iterator& other) { return node == other.node; }
+        inline bool       operator!=(const_iterator& other) { return node != other.node; }
+        inline node<T,D>& operator* ()                      { return **node;             }
+        inline node<T,D>* operator->()                      { return  *node;             }
+
+        node<T,D>** node;
+    };
+
     node() { for (int i = 0; i < D; ++i ) nodes[i] = NULL; }
    ~node() { for (int i = 0; i < D; ++i ) if (nodes[i]) delete nodes[i]; }
 
-    inline node<T,D>*       begin()                         { return children[0];     }
-    inline const node<T,D>* begin()                   const { return children[0];     }
-    inline node<T,D>*       end()                           { return children[D - 1]; }
-    inline const node<T,D>* end()                     const { return children[D - 1]; }
-    inline node<T,D>*       operator[](size_t& index)       { return children[index]; }
-    inline const node<T,D>* operator[](size_t& index) const { return children[index]; }
-    inline std::size_t      size()                          { return D;               }
-    
+    inline node<T,D>*       front     ()                               { return nodes[0];      }
+    inline const node<T,D>* front     ()                         const { return nodes[0];      }
+    inline node<T,D>*       back      ()                               { return nodes[D - 1];  }
+    inline const node<T,D>* back      ()                         const { return nodes[D - 1];  }
+    inline node<T,D>*       operator[](const std::size_t& index)       { return nodes[index];  }
+    inline const node<T,D>* operator[](const std::size_t& index) const { return nodes[index];  }
+    inline std::size_t      size      ()                               { return D;             }
+
+    inline iterator         begin     ()                               { iterator iter; iter.node = nodes;     return iter; }
+    inline iterator         end       ()                               { iterator iter; iter.node = nodes + D; return iter; }
+
     T* value;
 
 private:
@@ -35,17 +67,3 @@ public:
     }
 
 };
-
-
-/* Tree Type ******************************************************************/
-template<typename T, std::size_t D>
-class tree {
-  
-public:
-    
-
-private:
-    node<T, D> root;
-
-};
-
--- oxtorne/source/TreeTest.cpp
+++ oxtorne/source/TreeTest.cpp
@@ -4,6 +4,19 @@
 #include <iostream>
 
 int main(int argc, char* argv) {
+    node<int, 3> _node;
+
+    for (int i = 0; i < 3; ++i)
+        std::cout << _node[i] << std::endl;
+
+    std::cout << (_node.leaf() ? "leaf" : "not a leaf") << std::endl;
+
+    node<int,3>::iterator n_iter = _node.begin();
+    node<int,3>::iterator n_end  = _node.end();
+
+    for (; n_iter != n_end; n_iter++) {
+        std::cout << &*n_iter << std::endl;
+    }
 
 
     system("PAUSE");
