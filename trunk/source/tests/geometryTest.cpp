cdeb9bf5d4692805a61acdf111fb69ac0d9d3432
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2009-03-08 17:44:35 +0000

    Unit tests will be added, OpenMesh will be referenced

---
+++ trunk/source/tests/geometryTest.cpp
@@ -0,0 +1,11 @@
+/*
+ *  geometryTest.cpp
+ *  oxtorne
+ *
+ *  Created by Markus F on 08.03.09.
+ *  Copyright 2009 Markus Friese. All rights reserved.
+ *
+ */
+
+#include "geometryTest.h"
+
---
+++ trunk/source/tests/geometryTest.h
@@ -0,0 +1,17 @@
+/*
+ *  geometryTest.h
+ *  oxtorne
+ *
+ *  Created by Markus F on 08.03.09.
+ *  Copyright 2009 Markus Friese. All rights reserved.
+ *
+ */
+
+#include <CPlusTest/CPlusTest.h>
+ 
+class MyTests : public TestCase
+{
+public:
+    MyTests(TestInvocation* invocation);
+    virtual ~MyTests();
+};
