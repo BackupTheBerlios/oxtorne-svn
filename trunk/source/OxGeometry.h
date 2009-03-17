e179a13ce398247231acfbd3a7d3b9d51e7a53c5
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2009-03-17 18:07:55 +0000

    Modifications ...

--- trunk/source/OxGeometry.h
+++ trunk/source/OxGeometry.h
@@ -126,86 +126,80 @@ public:
 };
 
 /* Factories ******************************************************************/
-inline template<typename T> point<T,2>     make_point(const T&, const T&);
-inline template<typename T> point<T,3>     make_point(const T&, const T&, const T&);
-inline template<typename T> vector<T,2>    make_vector(const T&, const T&);
-inline template<typename T> vector<T,3>    make_vector(const T&, const T&, const T&);
-inline template<typename T> sphere<T,3>    make_sphere(const T&, const T&, const T&, const T&);
-inline template<typename T> sphere<T,3>    make_sphere(const point<T,3>&, const T&);
-inline template<typename T> ray<T,3>       make_ray(const T&, const T&, const T&, const T&, const T&, const T&);
-inline template<typename T> ray<T,3>       make_ray(const point<T,3>&, const vector<T,3>&);
-inline template<typename T> plane<T,3>     make_plane(const point<T,3>&, const point<T,3>&, const point<T,3>&);
-inline template<typename T> plane<T,3>     make_plane(const point<T,3>&, const vector<T,3>&);
-inline template<typename T> box<T,2>       make_box(const point<T,2>&, const point<T,2>&);
-inline template<typename T> box<T,2>       make_box(const T&, const T&, const T&, const T&);
-inline template<typename T> box<T,3>       make_box(const point<T,3>&, const point<T,3>&);
-inline template<typename T> box<T,3>       make_box(const T&, const T&, const T&, const T&, const T&, const T&);
-inline template<typename T> line<T,3>      make_line(const point<T,3>&, const point<T,3>&);
-inline template<typename T> line<T,3>      make_line(const T&, const T&, const T&, const T&, const T&, const T&);
-inline template<typename T> triangle<T,3>  make_triangle(const point<T,3>&, const point<T,3>&, const point<T,3>&);
-inline template<typename T> triangle<T,3>  make_triangle(const T&, const T&, const T&, const T&, const T&, const T&, const T&, const T&, const T&);
+template<typename T> inline point<T,2>     make_point(const T&, const T&);
+template<typename T> inline point<T,3>     make_point(const T&, const T&, const T&);
+template<typename T> inline vector<T,2>    make_vector(const T&, const T&);
+template<typename T> inline vector<T,3>    make_vector(const T&, const T&, const T&);
+template<typename T> inline sphere<T,3>    make_sphere(const T&, const T&, const T&, const T&);
+template<typename T> inline sphere<T,3>    make_sphere(const point<T,3>&, const T&);
+template<typename T> inline ray<T,3>       make_ray(const T&, const T&, const T&, const T&, const T&, const T&);
+template<typename T> inline ray<T,3>       make_ray(const point<T,3>&, const vector<T,3>&);
+template<typename T> inline plane<T,3>     make_plane(const point<T,3>&, const point<T,3>&, const point<T,3>&);
+template<typename T> inline plane<T,3>     make_plane(const point<T,3>&, const vector<T,3>&);
+template<typename T> inline box<T,2>       make_box(const point<T,2>&, const point<T,2>&);
+template<typename T> inline box<T,2>       make_box(const T&, const T&, const T&, const T&);
+template<typename T> inline box<T,3>       make_box(const point<T,3>&, const point<T,3>&);
+template<typename T> inline box<T,3>       make_box(const T&, const T&, const T&, const T&, const T&, const T&);
+template<typename T> inline line<T,3>      make_line(const point<T,3>&, const point<T,3>&);
+template<typename T> inline line<T,3>      make_line(const T&, const T&, const T&, const T&, const T&, const T&);
+template<typename T> inline triangle<T,3>  make_triangle(const point<T,3>&, const point<T,3>&, const point<T,3>&);
+template<typename T> inline triangle<T,3>  make_triangle(const T&, const T&, const T&, const T&, const T&, const T&, const T&, const T&, const T&);
 
 /* Algebraic Operations *******************************************************/
-inline template<typename T, size_t D> bool        operator==(const point<T,D>&, const point<T,D>&);
-inline template<typename T, size_t D> point<T,D>  operator+ (const point<T,D>&, const point<T,D>&);
-inline template<typename T, size_t D> vector<T,D> operator+ (const vector<T,D>&, const vector<T,D>&);
-inline template<typename T, size_t D> vector<T,D> operator- (const point<T,D>&, const point<T,D>&);
-inline template<typename T, size_t D> point<T,D>  operator* (const point<T,D>&, const T&);
-inline template<typename T, size_t D> vector<T,D> operator* (const vector<T,D>&, const T&);
-inline template<typename T, size_t D> point<T,D>  operator* (const T&, const point<T,D>&);
-inline template<typename T, size_t D> vector<T,D> operator* (const T&, const vector<T,D>&);
+template<typename T, size_t D> inline bool        operator==(const point<T,D>&, const point<T,D>&);
+template<typename T, size_t D> inline point<T,D>  operator+ (const point<T,D>&, const point<T,D>&);
+template<typename T, size_t D> inline vector<T,D> operator+ (const vector<T,D>&, const vector<T,D>&);
+template<typename T, size_t D> inline vector<T,D> operator- (const point<T,D>&, const point<T,D>&);
+template<typename T, size_t D> inline point<T,D>  operator* (const point<T,D>&, const T&);
+template<typename T, size_t D> inline vector<T,D> operator* (const vector<T,D>&, const T&);
+template<typename T, size_t D> inline point<T,D>  operator* (const T&, const point<T,D>&);
+template<typename T, size_t D> inline vector<T,D> operator* (const T&, const vector<T,D>&);
 
 /* Containment Checks *********************************************************/
-inline template<typename T, size_t D> bool box_in_box(const box<T,D>&, const box<T,D>&);
-
-/* Equalty Checks *************************************************************/
-inline template<typename T> bool           is_equal(const T&, const T&);
-inline template<typename T> bool           is_equal_or_smaller(const T&, const T&);
-inline template<typename T> bool           is_equal_or_greater(const T&, const T&);
+template<typename T, size_t D> inline bool box_in_box(const box<T,D>&, const box<T,D>&);
 
 /* Vector Calculations ********************************************************/
-inline template<typename T> T              length(const vector<T,3>&);
-inline template<typename T> T              distance(const point<T,3>&, const point<T,3>&);
-inline template<typename T> vector<T,3>    normalize(const vector<T,3>&);
-inline template<typename T> vector<T,3>    cross_product(const vector<T,3>&, const vector<T,3>&);
-inline template<typename T> T              dot_product(const point<T,3>&, const point<T,3>&);
+template<typename T> inline T              length(const vector<T,3>&);
+template<typename T> inline T              distance(const point<T,3>&, const point<T,3>&);
+template<typename T> inline vector<T,3>    normalize(const vector<T,3>&);
+template<typename T> inline vector<T,3>    cross_product(const vector<T,3>&, const vector<T,3>&);
+template<typename T> inline T              dot_product(const point<T,3>&, const point<T,3>&);
 
 /* Containment Checks *********************************************************/
-inline template<typename T> bool           point_in_box(const box<T,2>&, const point<T,2>&);
-inline template<typename T> bool           point_in_box(const box<T,3>&, const point<T,3>&);
-inline template<typename T> bool           point_in_box(const box<T,3>&, const T&, const T&, const T&);
+template<typename T> inline bool           point_in_box(const box<T,2>&, const point<T,2>&);
+template<typename T> inline bool           point_in_box(const box<T,3>&, const point<T,3>&);
+template<typename T> inline bool           point_in_box(const box<T,3>&, const T&, const T&, const T&);
 
 /* Itersection Checks *********************************************************/
-inline template<typename T> bool           intersect(const box<T,3>&, const triangle<T,3>&);
-inline template<typename T> bool           intersect(const line<T,3>&, const plane<T,3>&);
-inline template<typename T> bool           intersect(const ray<T,3>&, const plane<T,3>&);
-inline template<typename T> bool           intersect(const triangle<T,3>&, const line<T,3>&);
-inline template<typename T> bool           intersect(const triangle<T,3>&, const ray<T,3>&);
-inline template<typename T> bool           intersect(const triangle<T,3>&, const sphere<T,3>&);
+template<typename T> inline bool           intersect(const box<T,3>&, const triangle<T,3>&);
+template<typename T> inline bool           intersect(const line<T,3>&, const plane<T,3>&);
+template<typename T> inline bool           intersect(const ray<T,3>&, const plane<T,3>&);
+template<typename T> inline bool           intersect(const triangle<T,3>&, const line<T,3>&);
+template<typename T> inline bool           intersect(const triangle<T,3>&, const ray<T,3>&);
+template<typename T> inline bool           intersect(const triangle<T,3>&, const sphere<T,3>&);
 
 /* Intersection Locations *****************************************************/
-inline template<typename T> point<T,3>     intersection_point(const line<T,3>&, const plane<T,3>&);
-inline template<typename T> point<T,3>     intersection_point(const ray<T,3>&, const plane<T,3>&);
-inline template<typename T> point<T,3>     intersection_point(const line<T,3>&, const triangle<T,3>&);
-inline template<typename T> void           intersection_point(const sphere<T,3>&, const ray<T,3>&, point<T,3>*&, int&);
+template<typename T> inline point<T,3>     intersection_point(const line<T,3>&, const plane<T,3>&);
+template<typename T> inline point<T,3>     intersection_point(const ray<T,3>&, const plane<T,3>&);
+template<typename T> inline point<T,3>     intersection_point(const line<T,3>&, const triangle<T,3>&);
+template<typename T> inline void           intersection_point(const sphere<T,3>&, const ray<T,3>&, point<T,3>*&, int&);
 
 /* Cleset Point Calculations **************************************************/
-inline template<typename T> point<T,3>     closest_point_on_ray_from_point(const ray<T,3>&, const point<T,3>&);
-inline template<typename T> point<T,3>     closest_point_on_line_from_point(const line<T,3>&, const point<T,3>&);
-inline template<typename T> point<T,3>     closest_point_on_plane_from_point(const plane<T,3>&, const point<T,3>&);
-inline template<typename T> point<T,3>     closest_point_on_triangle_from_point(const triangle<T,3>&, const point<T,3>&);
+template<typename T> inline point<T,3>     closest_point_on_ray_from_point(const ray<T,3>&, const point<T,3>&);
+template<typename T> inline point<T,3>     closest_point_on_line_from_point(const line<T,3>&, const point<T,3>&);
+template<typename T> inline point<T,3>     closest_point_on_plane_from_point(const plane<T,3>&, const point<T,3>&);
+template<typename T> inline point<T,3>     closest_point_on_triangle_from_point(const triangle<T,3>&, const point<T,3>&);
 
 /* Bounding Sphere Calculations ***********************************************/
-inline template<typename T> sphere<T,3>    minimum_bounding_sphere(const triangle<T,3>&);
-inline template<typename T> sphere<T,3>    minimum_bounding_sphere(const box<T,3>&);
+template<typename T> inline sphere<T,3>    minimum_bounding_sphere(const triangle<T,3>&);
+template<typename T> inline sphere<T,3>    minimum_bounding_sphere(const box<T,3>&);
 
 /* Mirror Positions On Geometric Objects **************************************/
-inline template<typename T> point<T,3>     mirror(const plane<T,3>&, const point<T,3>&);
-inline template<typename T> vector<T,3>    mirror(const plane<T,3>&, const vector<T,3>&);
+template<typename T> inline point<T,3>     mirror(const plane<T,3>&, const point<T,3>&);
+template<typename T> inline vector<T,3>    mirror(const plane<T,3>&, const vector<T,3>&);
 
 };
 
 #include "OxGeometry.cpp"
 
 #endif
-inline 
