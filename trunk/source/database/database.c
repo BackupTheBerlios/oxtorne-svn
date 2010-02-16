86a4ff983c93e410ae65f90699f74c7358c72b85
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2010-02-16 07:53:32 +0000

    database functions extended

--- trunk/source/database/database.c
+++ trunk/source/database/database.c
@@ -28,17 +28,29 @@
 #include "stdlib.h"
 
 
-int db_exec( sqlite3* db, const char* query, ... )
+
+int db_prepare( sqlite3* db, sqlite3_stmt** pStmt, const char* query, ... )
 {
-	// SQliteStatement
-   sqlite3_stmt* pStmt;
+   // SQliteStatement
+   va_list vl;
+   size_t index;
+   int ii, rc;
 	
    // platz,um den string zu kopieren
-   char* buffer = (char*) malloc( strlen(query + 1) * sizeof(char) );
-   strcpy( buffer, query );
+   char* buffer = (char*) malloc( (strlen(query) + 1) * sizeof(char) );
+   memcpy( buffer, query, strlen(query) + 1 );
+
+   if ( !pStmt )
+   {
+      // Buffer wird wohl nicht mehr benotigt
+      free( buffer );
+
+      // Einen Fehler melden
+      return SQLITE_ERROR;
+   }
 
    // build a sqlite query
-   for ( int ii = 0; buffer[ii] != '\0'; ++ii )
+   for ( ii = 0; buffer[ii] != '\0'; ++ii )
    {
       // % symbol check
       if ( buffer[ii] == '%' && buffer[ii + 1] == '%' )
@@ -56,13 +68,151 @@ int db_exec( sqlite3* db, const char* query, ... )
    }
 
    // prepare the prepared query
-   int rc = sqlite3_prepare( db, buffer, -1, &pStmt, NULL );
+   rc = sqlite3_prepare( db, buffer, -1, pStmt, NULL );
 	
 	// check for errors
-	/*if ( SQLITE_ERROR == rc )
+	if ( SQLITE_ERROR == rc )
 	{
 		// prompt an error
 		printf( "Execution failed with error:\n%s\n", sqlite3_errmsg( db ) );
+      free( buffer );
+		
+		if ( NULL != *pStmt )
+		{
+			// Statement in memory?
+			sqlite3_free( *pStmt );
+		}
+		
+		// return an error
+		return SQLITE_ERROR;
+	}
+	
+	// good, no errors. continue with the variable argument list ...
+	va_start(vl, query);
+	
+	// counting the current binding position
+	index = 0;
+	
+	// we will check abort criteria inside of loop
+	for ( ii = 0; query[ii] != '\0'; ++ii )
+	{
+		// % symbol check
+      if ( query[ii] == '%' && query[ii + 1] == '%' )
+      {
+         // skip the next %
+         ++ii;
+         continue;
+      }
+
+      // Integer Check
+      if ( query[ii] == '%' && query[ii + 1] == 'i' )
+      {
+			// next argument is of type int
+         sqlite3_bind_int( *pStmt, ++index, va_arg( vl, int ) );
+         continue;
+      }
+
+      // String check
+      if ( query[ii] == '%' && query[ii + 1] == 's' )
+      {
+         // sqlite will not free the string
+         sqlite3_bind_text( *pStmt, ++index, va_arg( vl, char* ), -1, NULL );
+         continue;
+      }
+
+      // Wide Character String check
+      if ( query[ii] == '%' && query[ii + 1] == 'w' )
+      {
+			// sqlite will not free the string
+         sqlite3_bind_text16( *pStmt, ++index, va_arg( vl, wchar_t* ), -1, NULL );
+         continue;
+      }
+
+      // Float check
+      if ( query[ii] == '%' && query[ii + 1] == 'f' )
+      {
+         sqlite3_bind_double( *pStmt, ++index, (double)va_arg( vl, float ) );
+         continue;
+      }
+
+      // Double check
+      if ( query[ii] == '%' && query[ii + 1] == 'F' )
+      {
+         sqlite3_bind_double( *pStmt, ++index, va_arg( vl, double ) );
+         continue;
+      }
+
+      if ( query[ii] == '%' && query[ii + 1] != '\0' )
+      {
+         fprintf( stderr, "%c%c Combination is unknown\n", query[ii], query[ii + 1] );
+      }
+	}
+	
+	// va_list completed
+	va_end(vl);
+
+   // Den Buffer frei geben
+   free( buffer );
+
+   // Alles okay
+   return SQLITE_OK;
+}
+
+
+
+int db_step( sqlite3_stmt* statement )
+{
+   return sqlite3_step( statement );
+}
+
+
+
+int db_finalize( sqlite3_stmt* statement )
+{
+   return sqlite3_finalize( statement );
+}
+
+
+
+int db_exec( sqlite3* db, const char* query, ... )
+{
+	// SQliteStatement
+   sqlite3_stmt* pStmt;
+   size_t index;
+   va_list vl;
+   int ii, rc;
+	
+   // platz,um den string zu kopieren
+   char* buffer = (char*) malloc( (strlen(query) + 1) * sizeof(char) );
+   memcpy( buffer, query, strlen(query) + 1 );
+
+   // build a sqlite query
+   for ( ii = 0; buffer[ii] != '\0'; ++ii )
+   {
+      // % symbol check
+      if ( buffer[ii] == '%' && buffer[ii + 1] == '%' )
+      {
+         buffer[ii + 1] = ' ';
+         continue;
+      }
+
+      // question mark replacement
+      if ( buffer[ii] == '%' )
+      {
+         buffer[ii] = '?';
+         buffer[ii + 1] = ' ';
+      }
+   }
+
+   // prepare the prepared query
+   rc = sqlite3_prepare( db, buffer, -1, &pStmt, NULL );
+	
+	// check for errors
+	if ( SQLITE_ERROR == rc )
+	{
+		// prompt an error
+		printf( "Execution failed with error:\n%s\n", sqlite3_errmsg( db ) );
+      free( buffer );
 		
 		if ( NULL != pStmt )
 		{
@@ -72,17 +222,16 @@ int db_exec( sqlite3* db, const char* query, ... )
 		
 		// return an error
 		return SQLITE_ERROR;
-	}*/
+	}
 	
 	// good, no errors. continue with the variable argument list ...
-	va_list vl;
 	va_start(vl, query);
 	
 	// counting the current binding position
-	size_t index = 0;
+	index = 0;
 	
 	// we will check abort criteria inside of loop
-	for ( int ii = 0; query[ii] != '\0'; ++ii )
+	for ( ii = 0; query[ii] != '\0'; ++ii )
 	{
 		// % symbol check
       if ( query[ii] == '%' && query[ii + 1] == '%' )
@@ -138,9 +287,6 @@ int db_exec( sqlite3* db, const char* query, ... )
 	
 	// va_list completed
 	va_end(vl);
-	
-   // Free the buffer again
-   free( buffer );
 
 	// start iterating with the prepared query
 	while( SQLITE_ROW == sqlite3_step( pStmt ) )
@@ -153,6 +299,149 @@ int db_exec( sqlite3* db, const char* query, ... )
 	{
 		printf( "Execution failed:\n%s\n%s\n", sqlite3_errmsg( db ), query );
 	}
+
+   // Finalize Iterator
+   sqlite3_finalize( pStmt );
+
+   // Free the buffer again
+   free( buffer );
 		
 	return SQLITE_OK;
 }
+
+
+
+/*int db_exec( sqlite3* db, const char* query, ... )
+{
+	// SQliteStatement
+   sqlite3_stmt* pStmt;
+   size_t index;
+   va_list vl;
+   int ii, rc;
+	
+   // platz,um den string zu kopieren
+   char* buffer = (char*) malloc( (strlen(query) + 1) * sizeof(char) );
+   memcpy( buffer, query, strlen(query) + 1 );
+
+   // build a sqlite query
+   for ( ii = 0; buffer[ii] != '\0'; ++ii )
+   {
+      // % symbol check
+      if ( buffer[ii] == '%' && buffer[ii + 1] == '%' )
+      {
+         buffer[ii + 1] = ' ';
+         continue;
+      }
+
+      // question mark replacement
+      if ( buffer[ii] == '%' )
+      {
+         buffer[ii] = '?';
+         buffer[ii + 1] = ' ';
+      }
+   }
+
+   // prepare the prepared query
+   rc = sqlite3_prepare( db, buffer, -1, &pStmt, NULL );
+	
+	// check for errors
+	if ( SQLITE_ERROR == rc )
+	{
+		// prompt an error
+		printf( "Execution failed with error:\n%s\n", sqlite3_errmsg( db ) );
+      free( buffer );
+		
+		if ( NULL != pStmt )
+		{
+			// Statement in memory?
+			sqlite3_free( pStmt );
+		}
+		
+		// return an error
+		return SQLITE_ERROR;
+	}
+	
+	// good, no errors. continue with the variable argument list ...
+	va_start(vl, query);
+	
+	// counting the current binding position
+	index = 0;
+	
+	// we will check abort criteria inside of loop
+	for ( ii = 0; query[ii] != '\0'; ++ii )
+	{
+		// % symbol check
+      if ( query[ii] == '%' && query[ii + 1] == '%' )
+      {
+         // skip the next %
+         ++ii;
+         continue;
+      }
+
+      // Integer Check
+      if ( query[ii] == '%' && query[ii + 1] == 'i' )
+      {
+			// next argument is of type int
+         sqlite3_bind_int( pStmt, ++index, va_arg( vl, int ) );
+         continue;
+      }
+
+      // String check
+      if ( query[ii] == '%' && query[ii + 1] == 's' )
+      {
+         // sqlite will not free the string
+         sqlite3_bind_text( pStmt, ++index, va_arg( vl, char* ), -1, NULL );
+         continue;
+      }
+
+      // Wide Character String check
+      if ( query[ii] == '%' && query[ii + 1] == 'w' )
+      {
+			// sqlite will not free the string
+         sqlite3_bind_text16( pStmt, ++index, va_arg( vl, wchar_t* ), -1, NULL );
+         continue;
+      }
+
+      // Float check
+      if ( query[ii] == '%' && query[ii + 1] == 'f' )
+      {
+         sqlite3_bind_double( pStmt, ++index, (double)va_arg( vl, float ) );
+         continue;
+      }
+
+      // Double check
+      if ( query[ii] == '%' && query[ii + 1] == 'F' )
+      {
+         sqlite3_bind_double( pStmt, ++index, va_arg( vl, double ) );
+         continue;
+      }
+
+      if ( query[ii] == '%' && query[ii + 1] != '\0' )
+      {
+         fprintf( stderr, "%c%c Combination is unknown\n", query[ii], query[ii + 1] );
+      }
+	}
+	
+	// va_list completed
+	va_end(vl);
+
+	// start iterating with the prepared query
+	while( SQLITE_ROW == sqlite3_step( pStmt ) )
+	{
+		// TODO: read return values
+	}
+		
+	// Error handling
+	if ( SQLITE_DONE != sqlite3_errcode( db ) )
+	{
+		printf( "Execution failed:\n%s\n%s\n", sqlite3_errmsg( db ), query );
+	}
+
+   // Finalize Iterator
+   sqlite3_finalize( pStmt );
+
+   // Free the buffer again
+   free( buffer );
+		
+	return SQLITE_OK;
+}*/
--- trunk/source/database/database.h
+++ trunk/source/database/database.h
@@ -1,4 +1,16 @@
 #include "sqlite3.h"
 #include "stdlib.h"
 
-int db_exec( sqlite3* db, const char* query, ... );
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int db_prepare( sqlite3*, sqlite3_stmt**, const char*, ... );
+int db_step( sqlite3_stmt* );
+int db_finalize( sqlite3_stmt* );
+
+int db_exec( sqlite3*, const char*, ... );
+
+#ifdef __cplusplus
+}
+#endif
