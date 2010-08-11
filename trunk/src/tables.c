7df43c0c1f11bd450d41eaf1a07f83f266263301
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2010-08-11 14:53:34 +0000

    small test application is growing ...

--- trunk/src/tables.c
+++ trunk/src/tables.c
@@ -1,5 +1,14 @@
+#include "stdio.h"
+#include "string.h"
 
-int main(int argc, char argv[])
+int main(int argc, char** argv)
 {
+   if ( 2 != argc )
+   {
+      char* exec = strrchr(argv[0], '\\');
+      printf("Usage: %s FILE\n", exec ? ++exec : argv[0]);
+      return 1;
+   }
+
    return 0;
 }
