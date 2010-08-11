a71e055c73357239f0cf3782aae80d531ac47f1b
mfriese <mfriese@186f3708-d44c-0410-a16f-cd643d4794ad>
2010-08-11 14:19:41 +0000

    new (mini)project added

--- trunk/include/sqlite3/sqlite3opt.h
+++ trunk/include/sqlite3/sqlite3opt.h
@@ -5,11 +5,7 @@
 extern "C" {
 #endif
 
-int db_prepare( sqlite3*, sqlite3_stmt**, const char*, ... );
-int db_step( sqlite3_stmt* );
-int db_finalize( sqlite3_stmt* );
-
-int db_exec( sqlite3*, const char*, ... );
+int opt_prepare( sqlite3*, sqlite3_stmt**, const char*, ... );
 
 #ifdef __cplusplus
 }
